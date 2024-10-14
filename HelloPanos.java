093e024d897c543331317a7479cc87e797e41900
pkatsigiannis <pkatsigiannis@york.citycollege.eu>
2024-10-14 10:08:24 +0300

    initial commit

---
+++ HelloPanos.java
@@ -0,0 +1,8 @@
+public class HelloPanos
+{   
+    public static void main (String[] args)
+    {   
+        System.out.println ("Hello Panos!\nHello PC!");
+        
+    }
+}
---
+++ StarSquare.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class StarSquare
+{   
+    public static void main (String[] args)
+    {   
+        JOptionPane.showMessageDialog(null, "**********\n**********\n**********\n**********");
+    }
+}
